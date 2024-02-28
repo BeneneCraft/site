6a0c54b55daa09bd3e4c8bbfccbbf744e6057849
BeneneCraft <124102348+BeneneCraft@users.noreply.github.com>
2024-02-28 23:20:44 +0200

    Create js.js

---
+++ ai/js.js
@@ -0,0 +1,18 @@
+
+  (function (w, d, s, o, f, js, fjs) {
+    w["botsonic_widget"] = o;
+    w[o] =
+      w[o] ||
+      function () {
+        (w[o].q = w[o].q || []).push(arguments);
+      };
+    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+    js.id = o;
+    js.src = f;
+    js.async = 1;
+    fjs.parentNode.insertBefore(js, fjs);
+  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
+  Botsonic("init", {
+    serviceBaseUrl: "https://api.botsonic.ai",
+    token: "06d5400b-afd7-44c8-9ee9-ecbe596b0bd2",
+  });
